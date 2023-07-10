f70a1f17a8d770c6a57360309414953eb22b5d29
Nikolai Lønberg <niklonberg@gmail.com>
2023-07-10 15:13:25 +0200

    added test content, changed divs amount to 16 from 10

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,9 @@
 const gridContainer = document.querySelector(".grid-container");
 
 function createGridDivs() {
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < 16; i++) {
       const div = document.createElement("div");
+      div.textContent = "test";
       document.body.appendChild(div);
   }
 }
