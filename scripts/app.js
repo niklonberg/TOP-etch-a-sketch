189d838124bf9c12820ceeb6bf24ff7d521598ab
Nikolai Lønberg <niklonberg@gmail.com>
2023-07-10 14:52:14 +0200

    added appending of div element

--- scripts/app.js
+++ scripts/app.js
@@ -3,5 +3,7 @@ const gridContainer = document.querySelector(".grid-container");
 function createGridDivs() {
   for (let i = 0; i < 10; i++) {
       const div = document.createElement("div");
+      document.body.appendChild(div);
   }
 }
+
