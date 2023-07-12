53c21e6fa0ea4f7fe9df6b3c98f5766f2054202a
Nikolai Lønberg <niklonberg@gmail.com>
2023-07-12 15:56:10 +0200

    set up square grid layout for divs for mobile layout

--- scripts/app.js
+++ scripts/app.js
@@ -3,7 +3,7 @@ const gridContainer = document.querySelector(".grid-container");
 function createGridDivs() {
   for (let i = 0; i < 256; i++) {
       const div = document.createElement("div");
-      div.textContent = "test";
+      // div.textContent = "test";
       div.classList.add("grid-item");
       gridContainer.appendChild(div);
   }
