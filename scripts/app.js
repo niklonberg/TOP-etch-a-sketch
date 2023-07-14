7a15d7262bd21497b27b9bfaaf4c8f3da3a0b4b6
Nikolai Lønberg <niklonberg@gmail.com>
2023-07-14 10:01:55 +0200

    add getDivAmount() & amount of divs input container reference, so that user can specify how big of a grid they want

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,11 @@
+const amountOfDivsInput = document.querySelector('#input-div')
 const gridContainer = document.querySelector(".grid-container");
 
+function getDivAmount() {
+}
+
 function createGridDivs() {
-  for (let i = 0; i < 256; i++) {
+  for (let i = 0; i < 20; i++) {
       const div = document.createElement("div");
       // div.textContent = "test";
       div.classList.add("grid-item");
