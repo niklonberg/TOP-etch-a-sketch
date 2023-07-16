6d031b56a35be38d476443d04cab6f0ed692dfb8
Nikolai Lønberg <niklonberg@gmail.com>
2023-07-16 12:52:01 +0200

    remove references to color mode buttons from mode manager

--- service/ModeManager.js
+++ service/ModeManager.js
@@ -3,10 +3,6 @@ class ModeManager {
     this.colorMode = true;
     this.rainbowMode = false;
     this.eraserMode = false;
-
-    this.colorModeBtn = document.getElementById('color-btn');
-    this.rainBowModeBtn = document.getElementById('rainbow-btn');
-    this.eraserModeBtn = document.getElementById('eraser-btn');
   }
 }
 
