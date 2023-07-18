0d11a449903d4a256dc0c3c79a4f60394663b627
Nikolai Lønberg <niklonberg@gmail.com>
2023-07-18 13:42:33 +0200

    adjusted color manager methods, so they only remove what they are supposed to

--- service/ColorManager.js
+++ service/ColorManager.js
@@ -3,20 +3,22 @@ class ColorManager {
   }
 
   colorMode(element) {
+    element.classList.remove("rainbow-color")
     element.classList.add("chosen-color")
   }
 
   rainbowMode(element) {
+    element.classList.remove("chosen-color")
     element.classList.add("rainbow-color")
   }
 
   eraserMode(element) {
     const elementClasses = Array.from(element.classList);
-    elementClasses.forEach(key => {
-      if (key === "grid-item") {
+    elementClasses.forEach(cssClass => {
+      if (cssClass === "grid-item" || cssClass === "outline") {
         return
       } else {
-        element.classList.remove(key)
+        element.classList.remove(cssClass)
       }
     })
   }
